24c3d0a9eba005802d02e124d66f71efa550c33a
alexkarari <k.alexander@gmail.com>
2018-03-24 14:29:58 +0300

    Add a service to the application that calls data from Github.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { RecordsService } from './records.service';
 
 
 import { AppComponent } from './app.component';
+import { RecordComponent } from './record/record.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordComponent,
+    HttpModule
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [RecordsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/records.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class RecordsService {
+
+  private username: string;
+  private clientid = 'f1d7b7788e46862e77dc';
+  private clientsecret = '54f68381e8556513ef2a663fe4d5d8eab5235e52';
+  
+  constructor(private http: Http) {
+    console.log('Service working...');
+this.username = 'alexkarari';
+   }
+getRecordData() {
+  return this.http.get('https://api.github.com/users/' + this.username + '?client_id='
+   + this.clientid + '&client_secret=' + this.clientsecret)
+   .map(res => res.json());
+}
+}
